8ba3e929922e369c49febd73c40676c88bd3a752
charitha95 <>
2020-06-05 19:23:50 +0530

    feature (repository-details): add repo detail component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Layout from './containers/Layout';
 import RepoFinder from './containers/RepoFinder';
 import Repositories from './containers/Repositories';
+import RepositoryDetails from './containers/RepositoryDetails';
 
 function App() {
   return (
     <Layout>
       <RepoFinder />
       <Repositories />
+      <RepositoryDetails />
     </Layout>
   );
 }
---
+++ src/containers/RepositoryDetails/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RepositoryDetails = () => {
+  return <section>
+    <h2>Welcome to project 1's  Reade me file</h2>
+    <h4>A super cool project by Someone</h4>
+    <p>
+      mark down content
+    </p>
+  </section>
+}
+
+export default RepositoryDetails;
