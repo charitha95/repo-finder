e7e211a67b68d6e9e5173632ca9331918c262f71
charitha95 <>
2020-06-06 10:53:04 +0530

    feature (repo-finder): style repo finder component

--- src/containers/RepoFinder/index.js
+++ src/containers/RepoFinder/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Button, Input } from '../../components/UI';
+import classes from './style.module.scss';
 
 const RepoFinder = () => {
-  return <section>
-    <h1>Hello!</h1>
-    <h3>Welcome to GitHub Repo Finder app.</h3>
-    <label>GitHub Username</label>
-    <Input type='text' placeholder='Enter Git user name' />
-    <Button>GO</Button>
+  return <section className={classes.repoFinder}>
+    <section className={classes.header}>
+      <h1>Hello!</h1>
+      <h3>Welcome to GitHub Repo Finder app.</h3>
+    </section>
+
+    <section className={classes.search}>
+      <Input type='text' placeholder='Enter Git user name' />
+      <Button>GO</Button>
+    </section>
   </section>
 }
 
