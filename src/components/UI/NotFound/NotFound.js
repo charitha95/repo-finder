b4b48f4528eb01aeb7e4ab02198f712826b5a7d7
charitha95 <>
2020-06-07 17:21:27 +0530

    feature (not-found): style component

--- src/components/UI/NotFound/NotFound.js
+++ src/components/UI/NotFound/NotFound.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import notFoundImage from '../../../assets/imgs/not_found.png';
+import classNames from './style.module.scss';
 
 export default function NotFound() {
-  return <p>not found</p>
+  return <section className={classNames.notFound}>
+    <figure>
+      <img src={notFoundImage} alt='notfound' />
+    </figure>
+    <h4>Sorry!</h4>
+    <p>couldn't find your repositories</p>
+  </section>
 }
