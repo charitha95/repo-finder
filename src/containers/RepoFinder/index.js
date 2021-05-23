3c5cee3b3eb18527e08903df443ee4c6c2b9e66d
charitha95 <charitha.exe@gmail.com>
2021-05-23 12:16:22 +0530

    feat (source-code): add source code link

--- src/containers/RepoFinder/index.js
+++ src/containers/RepoFinder/index.js
@@ -1,37 +1,52 @@
-import React, { useState } from 'react';
-import classNames from './style.module.scss';
-import { Button, Input, Card } from '../../components/UI';
+import React, { useState } from "react";
+import classNames from "./style.module.scss";
+import githubIcon from "../../assets/icons/github.png";
+import { Button, Input, Card } from "../../components/UI";
 
 /**
- * Function represents the repo finder component 
- * @param {Object} props Component Props 
+ * Function represents the repo finder component
+ * @param {Object} props Component Props
  */
 export default function RepoFinder(props) {
-
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
 
   const handleClick = () => {
-    if (!username) { alert('invalid user name'); return }
-    props.history.push(`/${username}`)
-  }
+    if (!username) {
+      alert("invalid user name");
+      return;
+    }
+    props.history.push(`/${username}`);
+  };
 
-  return <section className={classNames.repoFinder}>
-    <section className={classNames.header}>
-      <h1>Hello!</h1>
-      <h3>Welcome to GitHub Repo Finder app.</h3>
+  return (
+    <section className={classNames.repoFinder}>
+      <section className={classNames.header}>
+        <h1>Hello!</h1>
+        <h3>Welcome to GitHub Repo Finder app.</h3>
+      </section>
+
+      <Card customClass={classNames.search}>
+        <Input
+          type="text"
+          placeholder="Enter Git user name"
+          changeHandler={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+
+        <Button clickHandler={handleClick}>GO</Button>
+      </Card>
+      <a
+        href="https://github.com/charitha95/repo-finder"
+        target="_blank"
+        className={classNames.sourceCode}
+        rel="noopener noreferrer" 
+      >
+        <h5>source code</h5>
+        <figure>
+          <img src={githubIcon} alt="source-code" />
+        </figure>
+      </a>
     </section>
-
-    <Card customClass={classNames.search}>
-      <Input
-        type='text'
-        placeholder='Enter Git user name'
-        changeHandler={(e) => { setUsername(e.target.value) }}
-      />
-
-      <Button
-        clickHandler={handleClick}>
-        GO
-      </Button>
-    </Card>
-  </section>
+  );
 }
